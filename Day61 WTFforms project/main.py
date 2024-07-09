d3d3ab4590b2280f2272f062d21a370975f6ef5d
walukaui <cwaluka@yahoo.com>
2024-07-09 10:35:21 -0500

    "validated WTF forms"

--- Day61 WTFforms project/main.py
+++ Day61 WTFforms project/main.py
@@ -31,9 +31,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
