8b39c9d22278195e7ce3befc6ee7a5e69570676c
walukaui <cwaluka@yahoo.com>
2024-07-16 09:10:26 -0500

    "Day64 files connected with anchor tags"

--- Day64_Movies/main.py
+++ Day64_Movies/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, URL
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -25,15 +25,60 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
 # CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[float] = mapped_column(Float, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(250), nullable=False, URL=True)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/add")
+def add():
+    # with app.app_context():
+    #     new_book = Book(id=2, title=request.form["title"], author=request.form["title"], rating=9.3)
+    #     db.session.add(new_book)
+    #     db.session.commit()
+    return render_template("add.html")
+
+
+@app.route("/update")
+def update():
+    return render_template("edit.html")
+
+
+@app.route("/delete")
+def delete():
+    return render_template("delete.html")
 
 
 if __name__ == '__main__':
