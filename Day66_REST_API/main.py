71b7936c2421163d36e03c8d596124b4b35fe101
walukaui <cwaluka@yahoo.com>
2024-07-17 14:06:27 -0500

    "day66 created 2  get requests"

--- Day66_REST_API/main.py
+++ Day66_REST_API/main.py
@@ -2,10 +2,19 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+import random
 
 '''
+Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
 On Windows type:
 python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
@@ -13,15 +22,12 @@ app = Flask(__name__)
 # CREATE DB
 class Base(DeclarativeBase):
     pass
-
-
-# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-# Cafe TABLE Configuration
+# CREATE TABLE
 class Cafe(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -35,6 +41,9 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -45,7 +54,20 @@ def home():
     return render_template("index.html")
 
 
-# HTTP GET - Read Record
+@app.route("/random")
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all")
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    new_list = [x.to_dict() for x in all_cafes]
+    return jsonify(new_list)
 
 # HTTP POST - Create Record
 
